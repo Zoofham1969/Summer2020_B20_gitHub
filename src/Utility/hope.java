837f6871bd74dcd025d74da5b9edacc93f7bf2ad
Zoofham1969 <parwani0602@gmail.com>
2020-08-05 20:16:56 -0500

    add hope.java

---
+++ src/Utility/hope.java
@@ -0,0 +1,15 @@
+package Utility;
+
+public class hope {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("I hope  tomorrow is going to be a nice day ");
+
+
+    }
+
+
+
+}
