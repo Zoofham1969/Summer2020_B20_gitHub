fcadc86d63c58b059c31818d61161e7f02e59fd3
Zoofham1969 <parwani0602@gmail.com>
2020-08-05 20:28:02 -0500

    add MissJava.java

---
+++ src/Utility/MissJava.java
@@ -0,0 +1,15 @@
+package Utility;
+
+public class MissJava {
+
+    public static void main(String[] args) {
+
+
+        //it is a nice day
+        System.out.println("Mahfooz");
+
+
+    }
+
+
+}
